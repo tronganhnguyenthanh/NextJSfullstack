7d02d46e230bcda826f6e8239e50968970e38caa
Trong Anh Nguyen Thanh <tronganhnguyen222@gmail.com>
2025-04-19 11:13:12 +0700

    [UPDATE][FS] build database in localhost

--- app/connectDB/connect.js
+++ app/connectDB/connect.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise"
 export const connectDB = async () => {
   const connection = await mysql.createConnection({
-    host:"sql12.freesqldatabase.com",
-    user:"sql12769132",
-    password:"L6X7WrwMqp",
-    database:"sql12769132"
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"project_domain_link"
   })
   return connection
 }
